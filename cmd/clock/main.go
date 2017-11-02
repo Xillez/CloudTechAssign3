6872eafe0fc5e62d44ecf267b02443b540e52df1
Kentwh <kent.wincent46@gmail.com>
2017-11-02 13:29:00 +0100

    Fixed clock/main.go

--- cmd/clock/main.go
+++ cmd/clock/main.go
@@ -17,21 +17,30 @@ var db = &mongodb.MongoDB{"mongodb://admin:admin@ds151452.mlab.com:51452/webhook
 
 func main() {
 
-	dayUpdated := time.Now().Format("2006-01-02")
-	for true /*dayUpdated != time.Now().Format("2006-01-02")*/ {
-		if dayUpdated != time.Now().Format("2006-01-02") {
+	dayUpdated := time.Now().AddDate(0, 0, -1).Format("2006-01-02")
+	didUpdate := false
+	durToSleep := time.Duration(0)
+	for true {
+		if dayUpdated != time.Now().Format("2006-01-02") && !(time.Now().Hour() < 17) {
 			log.Println(Info + "--------------- Updating currency ---------------")
 			db.UpdateCurr()
 			log.Println(Info + "--------------- Invoking webhooks ---------------")
 			db.InvokeWebhooks(true)
+
+			dayUpdated = time.Now().Format("2006-01-02")
+			didUpdate = true
 		}
 
 		log.Println(Info + "--------------- Sleeping ---------------")
 		timeLoc, _ := time.LoadLocation("CET")
-
-		//fmt.Println("-------- TIME UNTIL TOMORROW 17:00!: " + time.Until(time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day()+1, 17, 00, 00, 00, timeLoc)).String())
+		if didUpdate {
+			durToSleep = time.Until(time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day()+1, 17, 00, 00, 00, timeLoc))
+		} else {
+			durToSleep = time.Until(time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 17, 00, 00, 00, timeLoc))
+		}
+		log.Println("[INFO] SLEEP DURATION: " + durToSleep.String())
 
 		// Sleep until 17:00 the next day
-		time.Sleep(time.Until(time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day()+1, 17, 00, 00, 00, timeLoc)))
+		time.Sleep(durToSleep)
 	}
 }
