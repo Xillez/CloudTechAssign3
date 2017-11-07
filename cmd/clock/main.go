e86d7d626c0a1b110f496f26bd50450b9b580dcc
Kentwh <kent.wincent46@gmail.com>
2017-11-07 16:35:15 +0100

    Added amore funcitonality to clock to keep web awake and print sleep time every minute

--- cmd/clock/main.go
+++ cmd/clock/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/Xillez/CloudTechAssign2/mongodb"
@@ -16,11 +17,19 @@ var Info = "[INFO]: "
 //var db = &mongodb.MongoDB{"mongodb://localhost", "Currencies", "webhook", "curr"}
 var db = &mongodb.MongoDB{"mongodb://admin:admin@ds151452.mlab.com:51452/webhook_curr", "webhook_curr", "webhook", "curr"}
 
+func keepAlive() {
+	for true {
+		http.Get("localhost:8080/exchange/") //"https://powerful-brushlands-93106.herokuapp.com/excange/")
+		time.Sleep(time.Minute * 15)
+	}
+}
+
 func main() {
+	go keepAlive()
 
 	dayUpdated := time.Now().AddDate(0, 0, -1).Format("2006-01-02")
+
 	didUpdate := false
-	durToSleep := time.Duration(0)
 	for true {
 		if dayUpdated != time.Now().Format("2006-01-02") && !(time.Now().Hour() < 17) {
 			didUpdate = false
@@ -33,16 +42,15 @@ func main() {
 			didUpdate = true
 		}
 
-		log.Println(Info + "--------------- Sleeping ---------------")
+		// Report time to sleep
 		timeLoc, _ := time.LoadLocation("CET")
 		if didUpdate {
-			durToSleep = time.Until(time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day()+1, 17, 00, 00, 00, timeLoc))
+			log.Println("[INFO] TIME TO CURRENCY UPDATE: " + time.Until(time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day()+1, 17, 0, 0, 0, timeLoc)).String())
 		} else {
-			durToSleep = time.Until(time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 17, 00, 00, 00, timeLoc))
+			log.Println("[INFO] TIME TO CURRENCY UPDATE: " + time.Until(time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 17, 0, 0, 0, timeLoc)).String())
 		}
-		log.Println("[INFO] SLEEP DURATION: " + durToSleep.String())
 
-		// Sleep until 17:00 the next day
-		time.Sleep(durToSleep)
+		// Sleep for 1 min
+		time.Sleep(time.Minute)
 	}
 }
