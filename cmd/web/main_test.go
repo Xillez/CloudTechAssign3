14101a0b925d7d828c26c58cbb67ee0c9f512638
Kentwh <kent.wincent46@gmail.com>
2017-11-13 18:13:10 +0100

    Giving up git complaining about non added changes to commit, so I no give a fuc... Non-finnished main_test.go

--- cmd/web/main_test.go
+++ cmd/web/main_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -18,54 +17,44 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-var testdb = &mongodb.MongoDB{"mongodb://localhost", "Testing", "testWeb", "testCurr"}
+//var testdb = &mongodb.MongoDB{"mongodb://localhost", "Testing", "testWeb", "testCurr"}
 var testWebhookPos = types.WebhookInfo{bson.NewObjectId(), "http://webhook:8080/something", "EUR", "NOK", 0.5, 2.0}
 var testWebhookNeg = types.WebhookInfo{bson.NewObjectId(), "http://webhook:8080/something", "CAD", "NOK", 0.5, 2.0}
 var testCurrPos = types.CurrencyInfo{bson.NewObjectId(), "EUR", "2017-01-01", map[string]float64{"NOK": 2.0}}
 var client = http.Client{}
 
-/*func Test_TestingSetup(t *testing.T) {
-	//DB = &mongodb.MongoDB{"mongodb://localhost", "Testing", "testWeb", "testCurr"}
-	//DB.Init()
-}*/
+func Test_TestingSetup(t *testing.T) {
+	DB = &mongodb.MongoDB{"mongodb://localhost", "Testing", "testWeb", "testCurr"}
+}
 
 // Positive test, ProcGetWebhook
 func Test_Pos_ProcGetWebhook(t *testing.T) {
 	// Initialize database
-	testdb.Init()
+	DB.Init()
 
 	fetchedWebhook := types.WebhookDisp{}
 
 	// Dial database
-	session, err := mgo.Dial(testdb.DatabaseURL)
+	session, err := mgo.Dial(DB.DatabaseURL)
 	if err != nil {
 		panic(err)
 	}
 	defer session.Close()
 
 	// Add testwebhok to database
-	err = session.DB(testdb.DatabaseName).C(testdb.WebCollName).Insert(testWebhookPos)
+	err = session.DB(DB.DatabaseName).C(DB.WebCollName).Insert(&testWebhookPos)
 	if err != nil {
 		t.Error("Failed adding test webhook to database")
 	}
 
-	// Make recorder and server for stesting
-	recorder := httptest.NewRecorder()
+	// Make server for testing
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		utils.CheckPrintErr(procGetWebHook(r.URL.Path, recorder), recorder)
-		fmt.Println("IN THE SERVER!")
+		utils.CheckPrintErr(procGetWebHook(r.URL.Path, w), w)
 	}))
 	defer server.Close()
 
 	// Get webhook from processing function
 	resp, err := http.Get(server.URL + "/exchange/" + testWebhookPos.ID.Hex())
-
-	byteSlice := []byte{}
-	resp.Body.Read(byteSlice)
-	fmt.Println("procGetWebHook: RESP BODY!!!!!!")
-	fmt.Println(byteSlice)
-	fmt.Println("procGetWebHook: RESP BODY END!!!!!!")
-
 	if err != nil {
 		t.Error("Failed to from the server! | Error: " + err.Error())
 	}
@@ -80,25 +69,25 @@ func Test_Pos_ProcGetWebhook(t *testing.T) {
 	}
 
 	// Clean up after testing
-	_ = session.DB(testdb.DatabaseName).C(testdb.WebCollName).DropCollection()
+	_ = session.DB(DB.DatabaseName).C(DB.WebCollName).DropCollection()
 }
 
 // Positive test, ProcAddWebhook
 func Test_Pos_ProcAddWebhook(t *testing.T) {
 	// Initialize database
-	testdb.Init()
+	DB.Init()
 
 	fetchedWebhook := types.WebhookInfo{}
 
 	// Dial database
-	session, err := mgo.Dial(testdb.DatabaseURL)
+	session, err := mgo.Dial(DB.DatabaseURL)
 	if err != nil {
 		panic(err)
 	}
 	defer session.Close()
 
-	// Make new recorder and server for testing
-	recorder := httptest.NewRecorder()
+	// Make server for testing
+	recorder := httptest.NewRecorder{}
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		utils.CheckPrintErr(procAddWebHook(r, recorder), recorder)
 	}))
@@ -119,18 +108,18 @@ func Test_Pos_ProcAddWebhook(t *testing.T) {
 	req.Header.Add("content-type", "application/json")
 
 	// Set POST request
-	_, err = client.Do(req)
+	resp, err := client.Do(req)
 	if err != nil {
 		t.Error("Failed to POST to  \"" + server.URL + "\" | Error: " + err.Error())
 	}
 
 	// Check to see if ID is the same
-	if strings.Contains(recorder.Body.String(), testWebhookPos.ID.Hex()) {
+	if strings.Contains(resp., testWebhookPos.ID.Hex()) {
 		t.Error("Names not equal")
 	}
 
 	// Try to fetch added webhook
-	errFind := session.DB(testdb.DatabaseName).C(testdb.WebCollName).Find(bson.M{"_id": testWebhookPos.ID}).One(&fetchedWebhook)
+	errFind := session.DB(DB.DatabaseName).C(DB.WebCollName).Find(bson.M{"_id": testWebhookPos.ID}).One(&fetchedWebhook)
 	if errFind != nil {
 		t.Error("Failed to fetch added webhook")
 	}
@@ -140,5 +129,5 @@ func Test_Pos_ProcAddWebhook(t *testing.T) {
 	}
 
 	// Clean up after testing
-	_ = session.DB(testdb.DatabaseName).C(testdb.WebCollName).DropCollection()
+	_ = session.DB(DB.DatabaseName).C(DB.WebCollName).DropCollection()
 }
